702191dea1b5e626155a2f364ada93e471f5099d
李天柱 <PeterLee@PeterdeMacBook-Pro.local>
2015-02-02 23:46:47 +0800

    add a new file

---
+++ ChatInputToolBar/ChatInputToolBar/class/LTZInputToolBarView.h
@@ -0,0 +1,13 @@
+//
+//  LTZInputToolBarView.h
+//  ChatInputToolBar
+//
+//  Created by  李天柱 on 15/2/2.
+//  Copyright (c) 2015年 Peter Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTZInputToolBarView : UIView
+
+@end
---
+++ ChatInputToolBar/ChatInputToolBar/class/LTZInputToolBarView.m
@@ -0,0 +1,21 @@
+//
+//  LTZInputToolBarView.m
+//  ChatInputToolBar
+//
+//  Created by  李天柱 on 15/2/2.
+//  Copyright (c) 2015年 Peter Lee. All rights reserved.
+//
+
+#import "LTZInputToolBarView.h"
+
+@implementation LTZInputToolBarView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
